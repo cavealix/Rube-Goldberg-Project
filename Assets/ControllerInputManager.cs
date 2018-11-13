d6cd7d34bf0216192b8ce44b480fa6fad67fdf08
Alix Cave <cavealix@gmail.com>
2018-11-13 15:31:14 -0600

    Object Menu
    Populate Object menu with prefabs and attach menu manager logic.
    
    Next:
    - Swipe menu
    - spawn object

--- Assets/ControllerInputManager.cs
+++ Assets/ControllerInputManager.cs
@@ -16,6 +16,15 @@ public class ControllerInputManager : MonoBehaviour {
     public GameObject player; //player 
     public LayerMask laserMask; //choose which layer raycast to collide with
     public float yNudge = 1f;
+
+    //Swipe
+	private float swipeSum;
+	private float touchLast;
+	private float touchCurrent;
+	private float distance;
+	private bool hasSwipedLeft;
+	private bool hasSwipedRight;
+	public ObjectMenuManager objectMenuManager;
 	
 	// Use this for initialization
 	void Start () {
@@ -26,14 +35,15 @@ public class ControllerInputManager : MonoBehaviour {
     }
 
 
-	// Update is called once per frame
+	// Teleport and Swipe Logic
 	void Update () {
 		device = SteamVR_Controller.Input((int)trackedObject.index);
+		Debug.Log(trackedObject.index);
 
-		//if (device.gameObject.name == "Controller (left)")
+		//Teleportation Logic (need to include logic to only allow for left controller)
 		//{
 			//deploy laser on press
-			if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+			if (device.GetPress(SteamVR_Controller.ButtonMask.Grip/*Touchpad*/))
 			{
 				laser.gameObject.SetActive(true);
 				teleportAimerObject.SetActive(true);
@@ -67,7 +77,7 @@ public class ControllerInputManager : MonoBehaviour {
 			}	
 		
 			//teleport on press up
-			if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+			if (device.GetPressUp(SteamVR_Controller.ButtonMask.Grip/*Touchpad*/))
 			{
 				laser.gameObject.SetActive(false);
 				teleportAimerObject.SetActive(false);
@@ -75,7 +85,79 @@ public class ControllerInputManager : MonoBehaviour {
 				player.transform.position = teleportLocation;
 			}
 		//}
+
+		//Swipe Logic for menu (need to include logic to only allow for right controller)
+		//set initial touch to 0
+		if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+		{
+			touchLast = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+		}
+
+		//detect touch
+		if(device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			touchCurrent = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+			//how much did finger move this frame?
+			distance = touchCurrent - touchLast;
+			touchLast = touchCurrent;
+			//total movement
+			swipeSum += distance;
+
+			if(!hasSwipedRight)
+			{
+				if(swipeSum > 0.5f)
+				{
+					swipeSum = 0;
+					SwipeRight();
+					hasSwipedLeft = true;
+					hasSwipedLeft = false;
+				}
+			}
+			if(!hasSwipedLeft)
+			{
+				if(swipeSum < -0.5f)
+				{
+					swipeSum = 0;
+					SwipeLeft();
+				}
+				hasSwipedLeft = true;
+				hasSwipedRight = false;
+			}
+		}
+
+		//reset variable if finger up 
+		if(device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+		{
+			swipeSum = 0;
+			touchCurrent = 0;
+			touchLast = 0;
+			hasSwipedLeft = false;
+			hasSwipedRight = false;
+		}
+
+		//Spawn object currently selected by menu
+		if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+		{
+			SpawnObject();
+		}
 	
+	}
+
+	void SpawnObject()
+	{
+		objectMenuManager.SpawnCurrentObject();
+	}
+
+	//Swipe
+	void SwipeLeft()
+	{
+		objectMenuManager.MenuLeft();
+		Debug.Log("SwipeLeft");
+	}
+	void SwipeRight()
+	{
+		objectMenuManager.MenuRight();
+		Debug.Log("SwipeRight");
 	}
 
 	//Hold Objects
