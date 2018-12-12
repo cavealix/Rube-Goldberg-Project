20f9320ac598daab5a9a19ada3c83c91a60a053f
Alix Cave <cavealix@gmail.com>
2018-12-12 16:33:15 -0600

    Speech Recognition
    Added basic speech recognition starter script to HMD "ears"

---
+++ Assets/Scripts/SpeechRecognition.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class SpeechRecognition : MonoBehaviour {
+
+	KeywordRecognizer keywordRecognizer;
+
+	Dictionary<string,System.Action> keywords = new Dictionary<string, System.Action>();
+
+	void Start()
+	{
+		keywords.Add("go", ()=>
+		{
+			GoCalled();
+		});
+
+		keywords.Add("start", ()=>
+		{
+			StartCalled();
+		});
+
+		keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+		//
+		keywordRecognizer.OnPhraseRecognized += KeywordRecognizerOnPhraseRecognized;
+		//initiate listening service
+		keywordRecognizer.Start();
+
+		print(keywords);
+	}
+
+	void KeywordRecognizerOnPhraseRecognized(PhraseRecognizedEventArgs args)
+	{
+		System.Action keywordAction;
+
+		if(keywords.TryGetValue(args.text, out keywordAction))
+		{
+			//call relevant function
+			keywordAction.Invoke();
+		}
+	}
+
+	void GoCalled()
+	{
+		print("Go Called");
+	}
+
+	void StartCalled()
+	{
+		print("Start Called");
+	}
+
+}
